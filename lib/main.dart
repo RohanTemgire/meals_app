3aecbd32e86650d6048e74b351cecacbc42783fe
RohanTemgire <temgirerrohan@gmail.com>
2022-02-01 21:16:36 +0530

    module 7.5

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+import './screens/tabs_screen.dart';
 import './screens/categories_meals_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/meal_detail_screen.dart';
@@ -36,12 +38,24 @@ class MyApp extends StatelessWidget {
       initialRoute: '/', //default is '/'
       routes: {
         '/': (context) =>
-            CategoriesScreen(), //this can also be used instead of using the above method for home routing
+            TabsScreen(), //this can also be used instead of using the above method for home routing
         // '/category-meals': (ctx) => CategoriesMealsScreen(),
         //here there can be spelling errors and bc of that error your app can break so there is another way producing the same output
         CategoriesMealsScreen.routeName: (ctx) => CategoriesMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
+
+      // onGenerateRoute: (settings) {
+      //this is a useull method when you have a highly flexible app and then you might miss to mention route,
+      //in such cases this 'onGenerateRoute' will help you such that if you do not mention any routes in the routes list, then this method will run
+      //   print(settings);
+      //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      // },
+
+      onUnknownRoute:
+          (settings) => //now this function here will excute if your app fails to load from the given routes list, and from onGenerateRoute
+              // this function is the last opt for flutter to show something on the screen if anything fails, or your app crashes.
+              MaterialPageRoute(builder: (ctx) => CategoriesScreen()),
     );
   }
 }
--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,19 +1,89 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
   // const MealDetailPage({ Key? key }) : super(key: key);
 
+  Widget buildSectionTitle(BuildContext context, title) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 15),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget buildContainer(BuildContext context, Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      height: 200,
+      width: 300,
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(5),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final mealID = ModalRoute.of(context)!.settings.arguments as String;
-    // final id = mealID['id'] ;
+    final meal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealID);
+    //firstwhere returns the element when it first finds the element and does not go further
     return Scaffold(
       appBar: AppBar(
-        title: Text('$mealID'),
+        title: Text('${meal.title}'),
       ),
-      body: Center(
-        child: Text('this is the id for the selected meal $mealID'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            buildSectionTitle(context, 'Ingredients'),
+            buildContainer(
+              context,
+              ListView.builder(
+                  itemBuilder: (ctx, index) => Card(
+                        color: Theme.of(context).accentColor,
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text(meal.ingredients[index]),
+                        ),
+                      ),
+                  itemCount: meal.ingredients.length),
+            ),
+            buildSectionTitle(context, 'Steps'),
+            buildContainer(
+              context,
+              ListView.builder(
+                itemBuilder: (ctx, index) => Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('# ${index + 1}'),
+                      ),
+                      title: Text(meal.steps[index]),
+                    ),
+                    Divider()
+                  ],
+                ),
+                itemCount: meal.steps.length,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
